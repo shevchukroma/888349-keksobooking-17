4b60bfe4708ce9497899f73806234b7b62bc6c07
shevchukroma <46864764+shevchukroma@users.noreply.github.com>
2019-06-30 23:04:07 +0300

    исправляет ошибку с координатами

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ function getAdverts() {
   for (var i = 0; i < 8; i++) {
     author = {avatar: 'img/avatars/user0' + (i + 1) + '.png'};
     offer = {type: offerTypes[getRandomInt(0, offerTypes.length)]};
-    location = {x: getRandomInt(0, 1200), y: getRandomInt(130, 630)};
+    location = {x: getRandomInt(0, 1200) - 65 / 2, y: getRandomInt(130, 630) - 65};
     adverts.push({author: author, offer: offer, location: location});
   }
   return adverts;
