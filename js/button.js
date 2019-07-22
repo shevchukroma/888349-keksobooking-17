7f2873de1f3bafe6e45061e43df15227de218b33
shevchukroma <46864764+shevchukroma@users.noreply.github.com>
2019-07-22 12:37:16 +0300

    исправляет Б10

--- js/button.js
+++ js/button.js
@@ -1,13 +1,14 @@
 'use strict';
 
 (function () {
+  var BUTTON_WIDTH = 62;
+  var BUTTON_HEIGHT = 84;
+
   window.getButtonWidth = function () {
-    var BUTTON_WIDTH = 62;
     return BUTTON_WIDTH;
   };
 
   window.getButtonHeight = function () {
-    var BUTTON_HEIGHT = 84;
     return BUTTON_HEIGHT;
   };
 })();
