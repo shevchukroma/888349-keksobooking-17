773f6d1f27752c030f720c0dd954c844cd5408a8
shevchukroma <46864764+shevchukroma@users.noreply.github.com>
2019-07-31 20:38:43 +0300

    удаляет data.js

--- js/backend.js
+++ js/backend.js
@@ -1,4 +1,6 @@
 'use strict';
+var errorPopup = document.querySelector('#error');
+window.getAdverts = function () {};
 
 (function () {
   window.load = function (succesLoad, errorLoad) {
@@ -15,4 +17,28 @@
     xhr.open('GET', link);
     xhr.send();
   };
+
+  var onSucces = function (data) {
+    window.getAdverts = function () {
+      return data;
+    }();
+  };
+
+  var onError = function () {
+    errorPopup.style.position = 'absolute';
+    errorPopup.textContent = 'ERROR';
+    errorPopup.style.fontSize = '40px';
+    errorPopup.style.textAlign = 'center';
+    errorPopup.style.color = 'red';
+    errorPopup.style.zIndex = '100';
+    errorPopup.style.top = '0';
+    errorPopup.style.left = '50%';
+    errorPopup.style.transform = 'translateX(-50%)';
+    errorPopup.style.backgroundColor = 'black';
+    errorPopup.style.display = 'block';
+    errorPopup.style.width = '200px';
+    errorPopup.style.height = '200px';
+  };
+
+  window.load(onSucces, onError);
 })();
