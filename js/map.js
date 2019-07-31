a28e355fad57dfabf085783d2d3996e4452ee8e4
shevchukroma <46864764+shevchukroma@users.noreply.github.com>
2019-07-31 20:45:47 +0300

    Удаляет data.js со второй попытки

--- js/map.js
+++ js/map.js
@@ -11,6 +11,8 @@
   var maxHeight = 630;
   var newButton;
   var offers = [];
+  var errorPopup = document.querySelector('#error');
+  window.getAdverts = function () {};
 
   mainButton.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
@@ -62,4 +64,28 @@
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   });
+
+  var onSucces = function (data) {
+    window.getAdverts = function () {
+      return data;
+    }();
+  };
+
+  var onError = function () {
+    errorPopup.style.position = 'absolute';
+    errorPopup.textContent = 'ERROR';
+    errorPopup.style.fontSize = '40px';
+    errorPopup.style.textAlign = 'center';
+    errorPopup.style.color = 'red';
+    errorPopup.style.zIndex = '100';
+    errorPopup.style.top = '0';
+    errorPopup.style.left = '50%';
+    errorPopup.style.transform = 'translateX(-50%)';
+    errorPopup.style.backgroundColor = 'black';
+    errorPopup.style.display = 'block';
+    errorPopup.style.width = '200px';
+    errorPopup.style.height = '200px';
+  };
+
+  window.load(onSucces, onError);
 })();
